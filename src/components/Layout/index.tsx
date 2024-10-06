da3de9b0e7264944042b397cbc0c9481b7036065
pdt1806 <nmphuc1806@gmail.com>
2024-10-06 14:16:09 -0700

    chore: :memo: concert done!

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
 
   return (
     <>
-      {modalNoti.element}
+      {/* {modalNoti.element} */}
       <ScrollRestoration />
       <Header />
       {/* <HeaderNoti /> */}
