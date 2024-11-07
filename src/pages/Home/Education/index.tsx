0001c301b514695c4f8f39fd234c256117a81f87
pdt1806 <nmphuc1806@gmail.com>
2024-11-06 16:46:50 -0800

    refactor: :sparkles: css supremacy

--- src/pages/Home/Education/index.tsx
+++ src/pages/Home/Education/index.tsx
@@ -4,33 +4,68 @@ import {
   Center,
   Flex,
   Grid,
+  Image,
   Overlay,
   SimpleGrid,
   Text,
   Title,
-  Transition,
   rem,
 } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
-import { useEffect, useState } from 'react';
+import classes from './index.module.css';
 
 const PRIMARY_COL_HEIGHT = rem(500);
+const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 1.2 - var(--mantine-spacing-md) / 1.2)`;
+const TERTIARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} - ${SECONDARY_COL_HEIGHT} - var(--mantine-spacing-md))`;
+
+const data = [
+  {
+    name: 'Antelope High School',
+    date: 'Nov 2022 - expected Jun 2026',
+    logo: 'images/components/antelope-high-school-logo.webp',
+    image: 'images/components/antelope-high-school.webp',
+  },
+  {
+    name: 'Tran Quoc Toan 1 Secondary School',
+    date: 'Sep 2021 - Oct 2022',
+    logo: 'images/components/tqt1-secondary-school-logo.webp',
+    image: 'images/components/tqt1-secondary-school.webp',
+  },
+  {
+    name: 'The Asian International School',
+    date: 'Aug 2014 - May 2021',
+    logo: 'images/components/the-asian-international-school-logo.webp',
+    image: 'images/components/the-asian-international-school.webp',
+  },
+];
+
+const schoolComponents = data.map((item) => (
+  <>
+    <Overlay radius="md" p="md" className={classes.overlay}>
+      <Flex direction="column" align="center" justify="center" h="100%">
+        <Image
+          src={item.logo}
+          alt={item.name}
+          style={{
+            height: '120px',
+            width: 'auto',
+            marginBottom: 'var(--mantine-spacing-md)',
+          }}
+        />
+        <Text fw="bold" fz="xl" c="white" ta="center">
+          {item.name}
+        </Text>
+        <Text size="sm" c="white" ta="center">
+          {item.date}
+        </Text>
+      </Flex>
+    </Overlay>
+    <BackgroundImage p="md" h="100%" src={item.image} radius="md" />
+  </>
+));
 
 export default function Education() {
   const isMobile = useMediaQuery('(max-width: 62em)');
-
-  const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 1.2 - var(--mantine-spacing-md) / 1.2)`;
-  const TERTIARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} - ${SECONDARY_COL_HEIGHT} - var(--mantine-spacing-md))`;
-
-  const [overlayAnHS, setOverlayAnHS] = useState(false);
-  const [overlayTQT1, setOverlayTQT1] = useState(false);
-  const [overlayAIS, setOverlayAIS] = useState(false);
-
-  useEffect(() => {
-    setOverlayAnHS(isMobile ?? false);
-    setOverlayTQT1(isMobile ?? false);
-    setOverlayAIS(isMobile ?? false);
-  }, [isMobile]);
 
   return (
     <Box mt="xl">
@@ -48,46 +83,7 @@ export default function Education() {
             h={!isMobile ? PRIMARY_COL_HEIGHT : SECONDARY_COL_HEIGHT}
             style={{ position: 'relative' }}
           >
-            <Transition
-              mounted={overlayAnHS}
-              transition="fade"
-              duration={200}
-              timingFunction="ease"
-            >
-              {(styles) => (
-                <Overlay
-                  radius="md"
-                  onMouseLeave={() => !isMobile && setOverlayAnHS(false)}
-                  style={styles}
-                  p="md"
-                >
-                  <Flex direction="column" align="center" justify="center" h="100%">
-                    <img
-                      src="images/components/antelope-high-school-logo.webp"
-                      alt="Antelope High School"
-                      style={{
-                        height: '120px',
-                        width: '180px',
-                        marginBottom: 'var(--mantine-spacing-md)',
-                      }}
-                    />
-                    <Text fw="bold" fz="xl" c="white" ta="center">
-                      Antelope High School
-                    </Text>
-                    <Text size="sm" c="white" ta="center">
-                      Nov 2022 - expected Jun 2026
-                    </Text>
-                  </Flex>
-                </Overlay>
-              )}
-            </Transition>
-            <BackgroundImage
-              p="md"
-              h="100%"
-              src="images/components/antelope-high-school.webp"
-              radius="md"
-              onMouseEnter={() => !isMobile && setOverlayAnHS(true)}
-            />
+            {schoolComponents[0]}
           </Box>
           <Grid gutter="md">
             <Grid.Col>
@@ -100,90 +96,12 @@ export default function Education() {
             </Grid.Col>
             <Grid.Col span={isMobile ? 12 : 6}>
               <Box h={SECONDARY_COL_HEIGHT} style={{ position: 'relative' }}>
-                <Transition
-                  mounted={overlayTQT1}
-                  transition="fade"
-                  duration={200}
-                  timingFunction="ease"
-                >
-                  {(styles) => (
-                    <Overlay
-                      radius="md"
-                      onMouseLeave={() => !isMobile && setOverlayTQT1(false)}
-                      style={styles}
-                      p="md"
-                    >
-                      <Flex direction="column" align="center" justify="center" h="100%">
-                        <img
-                          src="images/components/tqt1-secondary-school-logo.webp"
-                          alt="Tran Quoc Toan 1 Secondary School"
-                          style={{
-                            height: '120px',
-                            width: '198.14px',
-                            marginBottom: 'var(--mantine-spacing-md)',
-                          }}
-                        />
-                        <Text fw="bold" fz="xl" c="white" ta="center">
-                          Tran Quoc Toan 1 Secondary School
-                        </Text>
-                        <Text size="sm" c="white" ta="center">
-                          Sep 2021 - Oct 2022
-                        </Text>
-                      </Flex>
-                    </Overlay>
-                  )}
-                </Transition>
-                <BackgroundImage
-                  p="md"
-                  h="100%"
-                  src="images/components/tqt1-secondary-school.webp"
-                  radius="md"
-                  onMouseEnter={() => !isMobile && setOverlayTQT1(true)}
-                />
+                {schoolComponents[1]}
               </Box>
             </Grid.Col>
             <Grid.Col span={isMobile ? 12 : 6}>
               <Box h={SECONDARY_COL_HEIGHT} style={{ position: 'relative' }}>
-                <Transition
-                  mounted={overlayAIS}
-                  transition="fade"
-                  duration={200}
-                  timingFunction="ease"
-                >
-                  {(styles) => (
-                    <Overlay
-                      radius="md"
-                      onMouseLeave={() => !isMobile && setOverlayAIS(false)}
-                      style={styles}
-                      p="md"
-                    >
-                      <Flex direction="column" align="center" justify="center" h="100%">
-                        <img
-                          src="images/components/the-asian-international-school-logo.webp"
-                          alt="The Asian International School"
-                          style={{
-                            height: '120px',
-                            width: '120px',
-                            marginBottom: 'var(--mantine-spacing-md)',
-                          }}
-                        />
-                        <Text fw="bold" fz="xl" c="white" ta="center">
-                          The Asian International School
-                        </Text>
-                        <Text size="sm" c="white" ta="center">
-                          Aug 2014 - May 2021
-                        </Text>
-                      </Flex>
-                    </Overlay>
-                  )}
-                </Transition>
-                <BackgroundImage
-                  p="md"
-                  h="100%"
-                  src="images/components/the-asian-international-school.webp"
-                  radius="md"
-                  onMouseEnter={() => !isMobile && setOverlayAIS(true)}
-                />
+                {schoolComponents[2]}
               </Box>
             </Grid.Col>
           </Grid>
